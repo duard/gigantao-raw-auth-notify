5cc79cc92d348acbaf97082de9a10ef14d4c7fed
Carlos Developer <duard.developer@gmail.com>
2025-11-08 11:17:15 -0300

    melhorar filtro client de funcnionarios

--- start-prod.sh
+++ start-prod.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Building production Docker image..."
-docker-compose -f docker-compose.prod.yml build
+docker compose -f docker-compose.prod.yml build
 
 echo "Starting production services..."
-docker-compose -f docker-compose.prod.yml up -d
+docker compose -f docker-compose.prod.yml up -d
 
 echo "Production services started."

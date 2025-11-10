4332ef5ee845712da7e2e32008c40aadda80e7bb
Carlos Developer <duard.developer@gmail.com>
2025-11-10 07:02:42 -0300

    good start

--- docker/mysql/init-prod-root.sql
+++ docker/mysql/init-prod-root.sql
@@ -4,5 +4,8 @@ CREATE DATABASE IF NOT EXISTS `gigantao_auth_notify_prod`;
 -- Create the dedicated user for the API and grant privileges
 CREATE USER IF NOT EXISTS 'auth_api'@'%' IDENTIFIED WITH caching_sha2_password BY 'senha123';
 GRANT ALL PRIVILEGES ON `gigantao_auth_notify_prod`.* TO 'auth_api'@'%';
+GRANT ALL PRIVILEGES ON `gigantao_auth_notify_prod`.* TO 'auth_api'@'10.0.1.6';
+GRANT ALL PRIVILEGES ON `gigantao_auth_notify_prod`.* TO 'auth_api'@'fd27:1fa:ace::6';
+GRANT ALL PRIVILEGES ON `gigantao_auth_notify_prod`.* TO 'auth_api'@'192.168.1.9';
 
 FLUSH PRIVILEGES;
